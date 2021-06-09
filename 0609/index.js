f241f3ace1e7b6adf8948892cb1fe0a8ee44364d
DevFrog92 <82052272+DevFrog92@users.noreply.github.com>
2021-06-10 00:03:18 +0900

    create : variable desc

--- 0609/index.js
+++ 0609/index.js
@@ -3,3 +3,6 @@
 // var, let, const (let과 const는 ES6부터 도입되었다.)
 // var : 함수 레벨 스코프(function-level scope)
 // let, const : 블록 레벨 스코프(block-level scope)
+
+// var 키워드를 사용한 변수 선언은 선언 단계와 초기화 단계가 동시에 진행된다.
+// 초기화 하지 않으면 garbage value에 의해서 정확한 값을 참조하지 못하는 경우가 발생한다.
