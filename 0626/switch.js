e317b91e51f4acbba48a6848215055dec6e09669
DevFrog92 <82052272+DevFrog92@users.noreply.github.com>
2021-06-27 01:17:32 +0900

    feat : What is switch and fall through?

---
+++ 0626/switch.js
@@ -0,0 +1,21 @@
+const month = 11;
+let monthName;
+
+switch(month){
+  case 1: monthName = '1월';
+  case 2: monthName = '2월';
+  case 3: monthName = '3월';
+  case 4: monthName = '4월';
+  case 5: monthName = '5월';
+  case 6: monthName = '6월';
+  case 7: monthName = '7월';
+  case 8: monthName = '8월';
+  case 9: monthName = '9월';
+  case 10: monthName = '10월';
+  case 11: monthName = '11월';
+  case 12: monthName = '12월';
+  default: monthName = 'Invalid month';
+}
+// 왜 11월이 출력되지 않고 Invalid month가 출력되는가?
+// Fall Through 때문에 발생한 문제이다. 
+console.log(monthName)
