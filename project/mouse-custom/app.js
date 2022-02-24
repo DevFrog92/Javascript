f2561793fc3cad15278b6bb2daa095494a9901c3
DevFrog92 <yongsu1481@gmail.com>
2022-02-24 14:53:44 +0900

    create mouse custom project

---
+++ project/mouse-custom/app.js
@@ -0,0 +1,21 @@
+const mouseCursor = document.querySelector('.cursor')
+const navLinks = document.querySelectorAll('.nav-links li')
+
+window.addEventListener('mousemove', cursor)
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + 'px'
+  mouseCursor.style.left = e.pageX + 'px'
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow')
+    link.classList.add("hover-link")
+  })
+
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow')
+    link.classList.remove("hover-link")
+  })
+})
