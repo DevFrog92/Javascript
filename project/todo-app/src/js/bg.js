281c5665c2b593050d38c808c3b90a7386828f76
DevFrog92 <yongsu1481@gmail.com>
2022-03-16 16:56:04 +0900

    add fetch api call for background images

--- project/todo-app/src/js/bg.js
+++ project/todo-app/src/js/bg.js
@@ -1,3 +1,7 @@
+const backgroundObject = {
+  backgroundElem: document.querySelector("body")
+}
+
 function getUnsplashKey() {
   const CLIENT_KEY = "VJjWn9mh-obXwt2PKiOTY-TjrsCA3yjS-xhS4fxGi4s";
 
@@ -7,6 +11,7 @@ function getUnsplashKey() {
 }
 
 function loadBackgroundImage() {
+  console.log("loadIm")
   const UNSPLASH_API_KEY = getUnsplashKey();
   const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
 
@@ -14,27 +19,50 @@ function loadBackgroundImage() {
   console.log(UNSPLASH_URL);
 }
 
-async function getBackgroundImage(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    if (data.urls && data.urls.full && data.location.city) {
-      const imageInfo = {
-        url: data.urls.full,
-        location: data.location,
-        city: data.location.city,
-        country: data.location.country,
-        name: data.location.name
-      }
+function setBackgroundImage(imageInfo) {
+  backgroundObject.backgroundElem.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url(${
+    imageInfo.url
+  })`;
+}
 
-      console.log(imageInfo);
-      return
-    }
+function saveBackground() {
 
-    getBackgroundImage(URL);
-  } catch (error) {
-    alert('Fail to load background image');
+}
+
+// async function getBackgroundImage(URL) {
+//   try {
+//     const response = await fetch(URL);
+//     const data = await response.json();
+//     if (data.urls && data.urls.full && data.location.city) {
+//       const imageInfo = {
+//         url: data.urls.full,
+//         location: data.location,
+//         city: data.location.city,
+//         country: data.location.country,
+//         name: data.location.name
+//       }
+
+//       console.log(imageInfo);
+//       // setBackgroundImage(imageInfo)
+//       return
+//     }
+
+//     // getBackgroundImage(URL);
+//   } catch (error) {
+//     // alert('Fail to load background image');
+//   }
+// }
+
+function getBackgroundImage(URL) {
+  const imageInfo = {
+    url: "https://images.unsplash.com/photo-1498550744921-75f79806b8a7?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMDg2MTN8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDY4MTU5MjY&ixlib=rb-1.2.1&q=85",
+    location: { city: "seoul"},
+    city: 'seoul',
+    country: "korea",
+    name: 'yong'
   }
+
+  setBackgroundImage(imageInfo)
 }
 
 function initBackground() {
